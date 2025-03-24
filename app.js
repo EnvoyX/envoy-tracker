0ed6850303864a99af15469ebe66fd509fd93c50
Muhamad Hanif Hafizhan <muhamadhanifhafizhan@gmail.com>
2025-03-24 23:45:25 +0700

    chore : setup for env config

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 import express from "express";
+import { PORT } from "./config/env.js";
 
 const app = express();
-const port = 3000;
 app.get("/", (req, res) => {
   res.send("Welcome to Envoy Subscription Tracker API!");
 });
 
-app.listen(port, () => {
+app.listen(PORT, () => {
   console.log(
-    `Envoy Subscription Tracker API is running on http://localhost:${port} `
+    `Envoy Subscription Tracker API is running on http://localhost:${PORT} `
   );
 });
 
