baa7a8fbdf77d4bac523fe2840137e0a09a730d9
Muhamad Hanif Hafizhan <muhamadhanifhafizhan@gmail.com>
2025-03-31 06:26:14 +0700

    feat : add authorized middleware to verify logged in user

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,17 +1,14 @@
 import { Router } from "express";
+import authorized from "../middlewares/auth.middleware.js";
+import { getUser, getUsers } from "../controllers/user.controller.js";
 
 const userRouter = Router();
 
 // GET /users -> get all users
 // GET /users/:id -> get user by id
 
-userRouter.get("/", (req, res) => {
-  res.send({ title: "GET all users" });
-});
-
-userRouter.get("/:id", (req, res) => {
-  res.send({ title: "GET user detail" });
-});
+userRouter.get("/", getUsers);
+userRouter.get("/:id", authorized, getUser);
 
 userRouter.post("/", (req, res) => {
   res.send({ title: "CREATE new users" });
